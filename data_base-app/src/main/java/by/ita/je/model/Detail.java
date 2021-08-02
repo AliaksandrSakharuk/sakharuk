528707ef9fa9a731b34f876a5517e0253d2d93d9
Aliaksandr Sakharuk <sakharukaliaksandr@gmail.com>
2021-08-02 12:49:11 +0300

    chchange dependencyes and controllers

--- data_base-app/src/main/java/by/ita/je/model/Detail.java
+++ data_base-app/src/main/java/by/ita/je/model/Detail.java
@@ -3,13 +3,18 @@ package by.ita.je.model;
 
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.ZonedDateTime;
 import java.util.Objects;
 
-
+@Entity
 public class Detail {
-
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String notes;
