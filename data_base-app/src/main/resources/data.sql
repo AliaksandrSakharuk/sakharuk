45f34b916fb293c017cb77169b9f9bf049d442a9
Aliaksandr Sakharuk <sakharukaliaksandr@gmail.com>
2021-09-01 11:55:45 +0300

    add INSERTs

--- data_base-app/src/main/resources/data.sql
+++ data_base-app/src/main/resources/data.sql
@@ -72,19 +72,40 @@ CREATE TABLE orders_worker (
 
 INSERT INTO orders(first_name_client, last_name_client, volume_bonus, phone_number, bill, is_cash)
 values ('OMA', 'HR', 20, 292020202, 1500.55, true);
+INSERT INTO orders(first_name_client, last_name_client, volume_bonus, phone_number, bill, is_cash)
+values ('MATERIK', 'SALES', 10, 291111111, 15.23, false);
+INSERT INTO orders(first_name_client, last_name_client, volume_bonus, phone_number, bill, is_cash)
+values ('MILE', 'SEQURITY', 50, 292222222, 1200, true);
 
 INSERT INTO car(name_owner, name_car, mileage, power,  is_electro, is_hibrid, order_id, DATA_TIME_START_FIX)
-values('IT-ACADEMY', 'ZAZ', 10000, 50, true, false, 1, sysdate());
+values('KOSTISHKO', 'ZAZ', 10000, 50, true, false, 1, sysdate());
+INSERT INTO car(name_owner, name_car, mileage, power,  is_electro, is_hibrid, order_id, DATA_TIME_START_FIX)
+values('GARMAZA', 'LADA', 1300, 50, false, false, 2, sysdate());
+INSERT INTO car(name_owner, name_car, mileage, power,  is_electro, is_hibrid, order_id, DATA_TIME_START_FIX)
+values('SELEDKA', 'GEELY', 500, 50, false, false, 3, sysdate());
 
 INSERT INTO detail (name, notes, PART_NUMBER, PURCHASE_PRICE, CAR_ID, DATA_TIME_DELIVERY)
 values('POWER', 'THE BEST OF THE BEST', 1234567, 50.0, 1, sysdate());
+INSERT INTO detail (name, notes, PART_NUMBER, PURCHASE_PRICE, CAR_ID, DATA_TIME_DELIVERY)
+values('PASS', 'CONTINENTAL', 838924, 100.0, 2, sysdate());
+INSERT INTO detail (name, notes, PART_NUMBER, PURCHASE_PRICE, CAR_ID, DATA_TIME_DELIVERY)
+values('KEY', 'BROLE DOWN ELECTRONIC', 123442, 55, 3, sysdate());
 
 INSERT INTO positions(status) values('MASTER');
+INSERT INTO positions(status) values('ASSISTANT');
+INSERT INTO positions(status) values('MECHANIC');
 
-INSERT INTO worker(second_name, first_name, salary, is_cheif, position_id)
-values ('KAZAK', 'VADIM', 10000.55, true, 1);
+INSERT INTO worker(second_name, first_name, salary, is_cheif, position_id, data_time_start_work)
+values ('KAZAK', 'VADIM', 10000.55, true, 1, '2015-09-01');
+INSERT INTO worker(second_name, first_name, salary, is_cheif, position_id, data_time_start_work)
+values ('SAKHARUK', 'ALIAKSANDR', 10.55, false, 2, '2018-01-09');
+INSERT INTO worker(second_name, first_name, salary, is_cheif, position_id, data_time_start_work)
+values ('SALAPURA', 'PETIA', 1000, false, 3, '2019-10-01');
+
 
 INSERT INTO orders_worker(order_id, worker_id) values(1,1);
+INSERT INTO orders_worker(order_id, worker_id) values(2,2);
+INSERT INTO orders_worker(order_id, worker_id) values(3,3);
 
 
 
