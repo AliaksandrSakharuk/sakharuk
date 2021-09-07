ba2f5dee852b5fb0217e10a764cf7f589b1dd93b
Aliaksandr Sakharuk <sakharukaliaksandr@gmail.com>
2021-09-07 11:41:11 +0300

    change hql_method

--- data_base-app/src/main/java/by/ita/je/model/Car.java
+++ data_base-app/src/main/java/by/ita/je/model/Car.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 @Entity
-@NamedQuery(name = "CarFindByOrdersBill", query = "SELECT car FROM Car car JOIN car.order JOIN car.order.listWorker worker " +
+@NamedQuery(name = "FindCarByHQL", query = "SELECT car FROM Car car JOIN car.order JOIN car.order.listWorker worker " +
         "WHERE (worker.dataTimeStartWork BETWEEN  :from_data AND :to_data) AND car.nameCar=:name")
 public class Car {
     @Id
