6225304d0f509d42c007346126d9eb0b61be5cd1
Aliaksandr Sakharuk <sakharukaliaksandr@gmail.com>
2021-09-24 14:57:26 +0300

    add model

---
+++ web-app/src/main/java/by/ita/je/model/Role.java
@@ -0,0 +1,20 @@
+package by.ita.je.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "role_name")
+    private String roleName;
+}
