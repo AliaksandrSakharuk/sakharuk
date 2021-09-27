afbabade743402ec3a184d0297e244c43fe19621
Aliaksandr Sakharuk <sakharukaliaksandr@gmail.com>
2021-09-27 19:52:25 +0300

    change sql

--- web-app/src/main/resources/security.sql
+++ web-app/src/main/resources/security.sql
@@ -35,6 +35,9 @@ INSERT INTO roles (role_name) VALUES ('READER');
 INSERT INTO roles (role_name) VALUES ('EDITOR');
 INSERT INTO roles (role_name) VALUES ('AUTHOR');
 
-INSERT INTO users_roles (user_id, role_id) VALUES (1, 3);
-INSERT INTO users_roles (user_id, role_id) VALUES (2, 3);
+INSERT INTO users_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO users_roles (user_id, role_id) VALUES (2, 1);
+INSERT INTO users_roles (user_id, role_id) VALUES (2, 2);
+INSERT INTO users_roles (user_id, role_id) VALUES (3, 1);
+INSERT INTO users_roles (user_id, role_id) VALUES (3, 2);
 INSERT INTO users_roles (user_id, role_id) VALUES (3, 3);
